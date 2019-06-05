a263c498700f5e00285bea5e3cb3c3084b31db2f
Kevin Phan <kphan0104@gmail.com>
2019-06-05 12:42:28 +0200

    Gitignore

--- Client/src/main/java/fr/dauphine/ar/application/ClientMain.java
+++ Client/src/main/java/fr/dauphine/ar/application/ClientMain.java
@@ -8,7 +8,7 @@ import fr.dauphine.ar.network.ClientHandleConnection;
 public class ClientMain {
 	public static void main(String[] args) {
 		int port = args.length == 1 ? Integer.parseInt(args[0]) : 1234;
-		String server = "localhost";
+		String server = "192.168.43.133";
 		try {
 			Socket s = new Socket(server, port);
 			ClientHandleConnection handler = new ClientHandleConnection(s);
