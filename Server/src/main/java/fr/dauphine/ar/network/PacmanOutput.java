899136f4981481dbb482d91b462a135ace7d5423
Kevin Phan <kphan0104@gmail.com>
2019-06-06 21:43:59 +0200

    21:43

--- Server/src/main/java/fr/dauphine/ar/network/PacmanOutput.java
+++ Server/src/main/java/fr/dauphine/ar/network/PacmanOutput.java
@@ -1,24 +1,23 @@
 package fr.dauphine.ar.network;
 
-import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 
 public class PacmanOutput implements PacmanProtocol {
 	
-	PrintWriter os;
+	private PrintWriter os;
 	
-	public PacmanOutput(OutputStream out) throws IOException {
+	public PacmanOutput(OutputStream out) {
 		this.os = new PrintWriter(out, true);
 	}
 	
-	public synchronized void size(int length, int width) {
+	public void size(int length, int width) {
 		os.println("SIZE");
 		os.println(length);
 		os.println(width);
 	}
 	
-	public synchronized void walls(boolean[][] walls) {
+	public void walls(boolean[][] walls) {
 		os.println("WALLS");
 		for(int i=0; i<walls.length; i++) {
 			for(int j=0; j<walls[0].length; j++) {
@@ -31,7 +30,7 @@ public class PacmanOutput implements PacmanProtocol {
 		os.println(".");
 	}
 	
-	public synchronized void pills(boolean[][] pills) {
+	public void pills(boolean[][] pills) {
 		os.println("PILLS");
 		for(int i=0; i<pills.length; i++) {
 			for(int j=0; j<pills[0].length; j++) {
@@ -44,34 +43,34 @@ public class PacmanOutput implements PacmanProtocol {
 		os.println(".");
 	}
 	
-	public synchronized void eat(int x, int y) {
+	public void eat(int x, int y) {
 		os.println("EAT");
 		os.println(x);
 		os.println(y);
 	}
 	
-	public synchronized void fruit(int x, int y, boolean isActive) {
+	public void fruit(int x, int y, boolean isActive) {
 		os.println("FRUIT");
 		os.println(isActive);
 		os.println(x);
 		os.println(y);
 	}
 	
-	public synchronized void nbPlayers(int nbPlayers) {
+	public void nbPlayers(int nbPlayers) {
 		os.println("NBPLAYERS");
 		os.println(nbPlayers);
 	}
 	
-	public synchronized void sendID(int id) {
+	public void sendID(int id) {
 		os.println("SENDID");
 		os.println(id);
 	}
 	
-	public synchronized void admin() {
+	public void admin() {
 		os.println("ADMIN");
 	}
 
-	public synchronized void pacman(int id, boolean isSuper, int x, int y, String direction, int score) {
+	public void pacman(int id, boolean isSuper, int x, int y, String direction, int score) {
 		os.println("PACMAN");
 		os.println(id);
 		if(!isSuper) os.println("NORMAL");
@@ -82,24 +81,24 @@ public class PacmanOutput implements PacmanProtocol {
 		os.println(score);
 	}
 	
-	public synchronized void pacmanDead(int id){
+	public void pacmanDead(int id){
 		os.println("PACMANDEAD");
 		os.println(id);
 	}
 	
-	public synchronized void ghost(int id, int x, int y){
+	public void ghost(int id, int x, int y){
 		os.println("GHOST");
 		os.println(id);
 		os.println(x);
 		os.println(y);
 	}
 	
-	public synchronized void ghostDead(int id) {
+	public void ghostDead(int id) {
 		os.println("GHOSTDEAD");
 		os.println(id);
 	}
 	
-	public synchronized void over() {
+	public void over() {
 		os.println("OVER");
 	}
 }
